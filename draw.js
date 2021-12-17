f0e872f5ebf866ee5e5dbf8172ca7dd62f22faba
Jacoby Joukema <jacoby.joukema@gmail.com>
2021-12-17 17:35:01 -0500

    fast

--- draw.js
+++ draw.js
@@ -2,17 +2,41 @@ var canvas = document.getElementById("canvas");
 
 var ctx = canvas.getContext("2d");
 
-console.log(shapes);
+draw();
 
-for (let shape of shapes) {
-  if (shape.type == "rect") {
-    ctx.fillStyle = shape.colour;
-    ctx.fillRect(shape.x, shape.y, shape.w, shape.h);
-    ctx.fillStyle = "black";
-  } else if (shape == "circle") {
-    ctx.fillStyle = shape.colour;
-    ctx.beginPath();
-    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-    ctx.fill();
+function draw() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  console.log(shapes);
+
+  for (let shape of shapes) {
+    if (shape.type == "rect") {
+      ctx.fillStyle = shape.colour;
+      ctx.fillRect(
+        shape.x * width,
+        shape.y * height,
+        shape.w * width,
+        shape.h * height
+      );
+      ctx.fillStyle = "black";
+    } else if (shape.type == "circle") {
+      console.log(shape);
+      ctx.fillStyle = shape.colour;
+      ctx.beginPath();
+      ctx.arc(
+        shape.x * width,
+        shape.y * height,
+        shape.r * Math.min(width, height),
+        0,
+        2 * Math.PI
+      );
+
+      ctx.fill();
+      ctx.fillStyle = "black";
+    }
   }
 }
--- shapes.js
+++ shapes.js
@@ -1,10 +1,17 @@
 shapes = [
   {
-    "type": "rect",
-    "x": 10,
-    "y": 10,
-    "w": 100,
-    "h": 50,
-    "colour": "blue"
-  }
-]
+    type: "rect",
+    x: 0.1,
+    y: 0.1,
+    w: 0.2,
+    h: 0.3,
+    colour: "blue",
+  },
+  {
+    type: "circle",
+    x: 0.5,
+    y: 0.5,
+    r: 0.2,
+    colour: "red",
+  },
+];
