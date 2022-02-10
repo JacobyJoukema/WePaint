645798ec3b5a620d538efe737c44f82e0b4d47f6
Jacoby Joukema <jacoby.joukema@gmail.com>
2022-02-10 16:44:24 -0500

    Updated with package-loc.json

--- client/src/constants/Config.ts
+++ client/src/constants/Config.ts
@@ -1,7 +1,7 @@
 const Config = {
   RESIZE_DEBOUNCE_DELAY: 200,
-  HTTP_API_ENDPOINT: "http://localhost:8000",
-  PARENT_DOMAIN: "localhost"
+  HTTP_API_ENDPOINT: "srv:8000",
+  PARENT_DOMAIN: "localhost",
 };
 
 export default Config;
