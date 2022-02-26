0e3437ccfc3fe35b52703bb11ef38f5f96ba9e0f
Jacoby Joukema <jacoby.joukema@gmail.com>
2022-02-25 22:37:33 -0500

    Updated domains

--- client/src/constants/Config.ts
+++ client/src/constants/Config.ts
@@ -1,7 +1,7 @@
 const Config = {
   RESIZE_DEBOUNCE_DELAY: 200,
-  HTTP_API_ENDPOINT: "34.122.118.14:8000",
-  PARENT_DOMAIN: "localhost",
+  HTTP_API_ENDPOINT: "https://api.wepaint.io",
+  PARENT_DOMAIN: "wepaint.io",
 };
 
 export default Config;
