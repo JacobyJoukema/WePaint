74c28b1ea0310cf81633254fbef76be351ab054d
Daniel Wolff <daniel.ryan.wolff@gmail.com>
2022-03-20 18:42:12 -0700

    addressing PR comments

--- srv/server.py
+++ srv/server.py
@@ -2,6 +2,7 @@ import json
 import os
 from typing import Optional
 from uuid import uuid4
+from datetime import datetime
 
 from database import (connect_database, create_circle_table, create_rect_table,
                       del_circle, del_rect, get_circles, get_rectangles,
@@ -10,24 +11,21 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 
-class Circle(BaseModel):
+class Shape(BaseModel):
     x: float
     y: float
+    colour: str
+    created_at: datetime
+
+
+class Circle(Shape):
     r: float
     colour: str
 
 
-class Rectangle(BaseModel):
-    x: float
-    y: float
+class Rectangle(Shape):
     w: float
     h: float
-    colour: str
-
-
-class Shapes(BaseModel):
-    circles: list[Circle]
-    rectangles: list[Rectangle]
 
 
 class ResponseID(BaseModel):
@@ -42,28 +40,27 @@ async def root() -> dict:
     return {}
 
 
-@app.get("/shapes", response_model=Shapes, status_code=200)
-async def get_shapes(beg_time: int = None, end_time: int = None) -> Shapes:
+@app.get("/shapes", response_model=list[Shape], status_code=200)
+async def get_shapes(beg_time: int = None, end_time: int = None) -> list[Shape]:
     with connect_database() as conn:
         circles = get_circles(conn, beg_time, end_time)
         rects = get_rectangles(conn, beg_time, end_time)
 
-        circle_resp = []
-        rect_resp = []
+        resp = []
         for circle in circles:
-            circle_resp.append(
+            resp.append(
                 {
                     "id": circle[0],
                     "colour": circle[1],
                     "x": circle[2],
                     "y": circle[3],
                     "r": circle[4],
-                    "created_at": circle[5],
+                    "created_at": int(circle[5].timestamp()),
                     "type": "circle",
                 }
             )
         for rect in rects:
-            rect_resp.append(
+            resp.append(
                 {
                     "id": rect[0],
                     "colour": rect[1],
@@ -71,13 +68,12 @@ async def get_shapes(beg_time: int = None, end_time: int = None) -> Shapes:
                     "y": rect[3],
                     "w": rect[4],
                     "h": rect[5],
-                    "created_at": rect[6],
+                    "created_at": int(rect[6].timestamp()),
                     "type": "rect",
                 }
             )
 
-        resp = circle_resp + rect_resp
-        resp.sort(lambda x: x.created_at)
+        resp.sort(lambda shape: shape["created_at"])
         return resp
 
 
